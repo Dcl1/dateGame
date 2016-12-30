b3e858cca488214b270db12fda254a4369d62290
DanCLee <dan.clee1218@gmail.com>
2016-12-30 00:14:39 -0800

    added thunk

--- src/app.js
+++ src/app.js
@@ -5,3 +5,13 @@ import thunk from 'redux-thunk';
 
 
 import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
+
+import * as reducers from './reducers';
+
+
+const reducer = storage.reducer(combineReducers(reducers));
+const engine = createEngine('dateApp1');
+const storageMiddleware = storage.createMiddleware(engine);
+
+let middleware = [thunk]
